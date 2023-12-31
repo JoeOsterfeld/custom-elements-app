52a7bbcc2ede95ec1d35ff98630abb23e731fd5f
Joe Osterfeld <joeosterfeld@gmail.com>
2023-12-31 10:00:09 -0500

    Added sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 const nodeResolve = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
 const terser = require('@rollup/plugin-terser');
+const sourceMaps = require('rollup-plugin-sourcemaps');
+
 
 const formats = ['esm', 'umd', 'cjs'];
 
@@ -9,11 +11,15 @@ module.exports = formats.map(format => ({
   output: {
     file: `dist/minifw.${format}.js`,
     format,
-    name: 'MiniFw'
+    name: 'MiniFw',
+    sourcemap: true,
   },
   plugins: [
+      sourceMaps(),
       nodeResolve(),
       commonjs(),
-      terser(),
+      terser({
+        sourceMap: true
+      }),
   ]
 }));
