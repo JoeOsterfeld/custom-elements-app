def81220bae9fe867a44052ec99e22c060d4a3cf
Joe Osterfeld <joeosterfeld@gmail.com>
2024-02-07 19:54:43 -0500

    Publish version 1.1.2

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-import { AppState } from './appState';
-import { MinElement } from './minElement';
-import { defineElement } from './defineElement';
-import {Router} from './router';
-import {el} from './elScope';
-
-(window as any).el = el;
-
-export default {
-  MinElement,
-  AppState,
-  defineElement,
-  Router
-};
+export {MinElement} from './minElement';
+export { AppState } from './appState';
+export {Router} from './router';
+export { defineElement } from './defineElement';
